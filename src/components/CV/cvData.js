d49a5b066a70a192a4553d4873f047196710af5c
Manyim Ma Makon <nmaknyim@gmail.com>
2025-07-16 22:43:51 +0200

    Ajout du projet personnel fruits-list

--- src/components/CV/cvData.js
+++ src/components/CV/cvData.js
@@ -78,6 +78,19 @@ const cvData = {
   ],
   personalProjects: [
     {
+      name: "Fruit Management App",
+      link: ["https://github.com/EdMkn/fruit-register-list"],
+      duration: "1 heure",
+      description: [
+        "La raison pour laquelle j'ai réalisé ce projet était pour me familiariser avec FastAPI, un framework web Python moderne et rapide (haute performance) pour construire des API.",
+        "Il s'agit d'une simple application web qui permet aux utilisateurs d'enregistrer des fruits. Le backend est construit avec FastAPI et fournit des endpoints pour ajouter et lister des fruits. Le frontend est construit avec React et interagit avec le backend pour afficher et enregistrer les fruits saisis par l'utilisateur."
+      ],
+      images: [
+        "images/fruits-list-1.png",
+        "images/fruits-list-2.png",
+      ],
+    },
+    {
       name: "Projet_Calcul",
       link: ["https://github.com/EdMkn/Projet_Calcul"],
       duration: "2 semaines",
